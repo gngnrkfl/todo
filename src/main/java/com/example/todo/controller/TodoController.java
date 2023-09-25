eef3113b419166ebd4b760185589cb297365a144
Kim Mun Pil <ansvlf0606@gmail.com>
2023-09-25 13:16:16 +0900

    Retrieve REST API 개발

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,38 +25,48 @@ import lombok.extern.slf4j.Slf4j;
 public class TodoController {
 	@Autowired
 	private TodoService service;
-	
+
 	@PostMapping
 	public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
 		try {
-			//POST localhost:8080/todo
+			// POST localhost:8080/todo
 			log.info("Log : Createtodo entrance");
-			
-			//dto를 이용해 테이블에 저장하기 위한 entity를 생성한다.
+
+			// dto를 이용해 테이블에 저장하기 위한 entity를 생성한다.
 			TodoEntity entity = TodoDTO.toEntity(dto);
 			log.info("Log:dto => entity ok!");
-			
-			//entity userId를 임시로 지정한다.
+
+			// entity userId를 임시로 지정한다.
 			entity.setUserId("temporary-userid");
-			
-			//service.create를 통해 repository에 entity를 저장한다.
-			//이떄 넘어오는 값이 없을수도 있으므로 List가 아닌 Optional로 한다.
+
+			// service.create를 통해 repository에 entity를 저장한다.
+			// 이떄 넘어오는 값이 없을수도 있으므로 List가 아닌 Optional로 한다.
 			Optional<TodoEntity> entities = service.create(entity);
 			log.info("Log: : service.create ok!");
-			
-			//entities를 dtos로 스트림 변환한다.
+
+			// entities를 dtos로 스트림 변환한다.
 			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
-			
-			//Response DTO를 생성한다.
+
+			// Response DTO를 생성한다.
 			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
 			log.info("Log : response dto ok!");
-			
-			//HTPP status 200 상태로 response를 전송한다.
+
+			// HTPP status 200 상태로 response를 전송한다.
 			return ResponseEntity.ok().body(response);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			String error = e.getMessage();
 			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
 			return ResponseEntity.badRequest().body(response);
 		}
+	}
+
+	@GetMapping
+	public ResponseEntity<?> retrieveTodoList() {
+		String temporaryUserId = "temporary-userid";
+		List<TodoEntity> entities = service.retrieve(temporaryUserId);
+		List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+		
+		return ResponseEntity.ok().body(response);
 	}
 }
