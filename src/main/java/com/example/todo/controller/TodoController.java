dd372c02e2bfdce7dd9db0056f02011211a73feb
Kim Mun Pil <ansvlf0606@gmail.com>
2023-09-25 14:03:11 +0900

    Update REST API 개발

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -66,7 +67,59 @@ public class TodoController {
 		List<TodoEntity> entities = service.retrieve(temporaryUserId);
 		List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
 		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
-		
+
 		return ResponseEntity.ok().body(response);
+	}
+
+	@GetMapping("/update")
+	public ResponseEntity<?> update(@RequestBody TodoDTO dto) {
+		try {
+			// dto를 이용해 테이블에 저장하기 위한 entity를 생성한다.
+			TodoEntity entity = TodoDTO.toEntity(dto);
+
+			// entity userId를 임시로 지정한다.
+			entity.setUserId("temporary-userid");
+
+			// service.create를 통해 repository에 entity를 저장한다.
+			// 이때 넘어오는 값이 없을수도 있으므로 List가 아닌 Optional을 쓴다.
+			Optional<TodoEntity> entities = service.update(entity);
+
+			// entities를 dtos로 스트림 변환한다.
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+			// Response DTO를 생성한다
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+			return ResponseEntity.ok().body(response);
+		} catch (Exception e) {
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}
+	}
+
+	@PutMapping
+	public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto) {
+		try {
+			// dto를 이용해 테이블에 저장하기 위한 entity를 생성한다.
+			TodoEntity entity = TodoDTO.toEntity(dto);
+
+			// entity userId를 임시로 지정한다.
+			entity.setUserId("temporary-userid");
+
+			// service.create를 통해 repository에 entity를 저장한다.
+			// 이때 넘어오는 값이 없을수도 있으므로 List가 아닌 Optional을 쓴다.
+			Optional<TodoEntity> entities = service.updateTodo(entity);
+
+			// entities를 dtos로 스트림 변환한다.
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+			// Response DTO를 생성한다
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+			return ResponseEntity.ok().body(response);
+		} catch (Exception e) {
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}
 	}
 }
